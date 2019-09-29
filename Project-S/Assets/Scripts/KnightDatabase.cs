55f5f73230a6e6f7279466bf568a516c6686dfad
bird <asd1535peaunts@gmail.com>
2019-09-29 10:13:09 +0900

    データベースの雛形をとりあえず作成、UIの色を全体的に調節

---
+++ Project-S/Assets/Scripts/KnightDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class KnightDatabase : ScriptableObject {
+
+    public AnimationCurve maxHP;
+    public AnimationCurve attack;
+    public AnimationCurve defense;
+    public AnimationCurve maxMP;
+
+
+    public int speed;
+
+}
