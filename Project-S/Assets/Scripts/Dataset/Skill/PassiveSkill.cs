d9675ce9d56ba9889487f9b7c80a97aaf5d8d273
superbirdyman <asada_mac@poslnoMacBook-puro.local>
2020-05-14 17:38:50 +0900

    add abstract skill class

---
+++ Project-S/Assets/Scripts/Dataset/Skill/PassiveSkill.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PassiveSkill : SkillBase {
+
+    //バトルが始まった時
+    public abstract void OnStartBattle();
+    //ターンが変わった時
+    public abstract void OnBeginTurn(Turn_State turn);
+}
---
+++ Project-S/Assets/Scripts/Dataset/Skill/SkillBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SkillBase : ScriptableObject {
+
+    KnightCore owner;
+
+    public void SetOwner(KnightCore core) {
+        owner = core;
+    }   
+
+
+}
